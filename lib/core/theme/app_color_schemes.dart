4eec0ddfcf4536fd8b2ff9d0c4f42e31625a819d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-11 17:40:16 +0600

    add app_color_schemes file

---
+++ lib/core/theme/app_color_schemes.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+import "package:task/generated/colors.gen.dart";
+
+class AppColorSchemes {
+  static final ColorScheme lightColorScheme = const ColorScheme.light(
+    primary: AppColors.primaryColor,
+    primaryContainer: AppColors.primaryContainerColor,
+    onError: AppColors.onErrorColor,
+    onPrimary: AppColors.onPrimaryColor,
+  );
+}
+
+class LightCodeColors {
+  Color get darkBlack => AppColors.fontDarkColor;
+
+  Color get slateGray => AppColors.fontSlateGray;
+
+  Color get stoneGray => AppColors.iconStoneGray;
+
+  Color get green => AppColors.greenColor;
+
+  Color get error => AppColors.errorColor;
+}
