4b6811274e70335e2887bada1143836285352dec
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-11 17:39:41 +0600

    add AppRoutes class

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+import "package:task/presentation/main/main_page.dart";
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String initialRoute = "/";
+
+  static Route? onGenerateRoute(RouteSettings settings) {
+    return switch (settings.name) {
+      initialRoute => _mainPageBuilder(),
+      _ => null,
+    };
+  }
+
+  static Route _mainPageBuilder() {
+    return MaterialPageRoute(
+      builder: (mainPageContext) {
+        return const MainPage();
+      },
+    );
+  }
+}
