ecff8dbfe06cfae01aa61d23444fad15577d7e17
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-11 15:16:31 +0600

    add generated file

---
+++ lib/generated/colors.gen.dart
@@ -0,0 +1,36 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  /// Color: #FF494C
+  static const Color errorColor = Color(0xFFFF494C);
+
+  /// Color: #DFE4F1
+  static const Color gradientRightColor = Color(0xFFDFE4F1);
+
+  /// Color: #009F76
+  static const Color greenColor = Color(0xFF009F76);
+
+  /// Color: #6D7491
+  static const Color onErrorColor = Color(0xFF6D7491);
+
+  /// Color: #0D101C
+  static const Color onPrimaryColor = Color(0xFF0D101C);
+
+  /// Color: #613BE7
+  static const Color primaryColor = Color(0xFF613BE7);
+
+  /// Color: #FFFFFF
+  static const Color primaryContainerColor = Color(0xFFFFFFFF);
+}
