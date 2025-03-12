72f39ef1ace584a8e99f8fa92e8943acce346409
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-12 11:04:18 +0600

    update main page import path

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:task/presentation/main/main_page.dart";
+import "package:task/presentation/pages/main_page.dart";
 
 class AppRoutes {
   AppRoutes._();
