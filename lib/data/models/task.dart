db2a0701a587010f9b98a032b9009b9558b3f71d
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-12 23:28:52 +0600

    add task model files

---
+++ lib/data/models/task.dart
@@ -0,0 +1,45 @@
+import "package:hive/hive.dart";
+import "package:task/domain/entity/task.dart";
+
+part "task.g.dart";
+
+@HiveType(typeId: 0)
+class TaskModel {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String title;
+
+  @HiveField(2)
+  final String description;
+
+  @HiveField(3)
+  final DateTime startDate;
+
+  @HiveField(4)
+  final DateTime endDate;
+
+  @HiveField(5)
+  final bool isCompleted;
+
+  TaskModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.startDate,
+    required this.endDate,
+    this.isCompleted = false,
+  });
+
+  factory TaskModel.fromEntity(TaskEntity entity) {
+    return TaskModel(
+      id: entity.id,
+      title: entity.title,
+      description: entity.description,
+      startDate: entity.startDate,
+      endDate: entity.endDate,
+      isCompleted: entity.isCompleted,
+    );
+  }
+}
---
+++ lib/data/models/task.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskModelAdapter extends TypeAdapter<TaskModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskModel(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      description: fields[2] as String,
+      startDate: fields[3] as DateTime,
+      endDate: fields[4] as DateTime,
+      isCompleted: fields[5] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.startDate)
+      ..writeByte(4)
+      ..write(obj.endDate)
+      ..writeByte(5)
+      ..write(obj.isCompleted);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
