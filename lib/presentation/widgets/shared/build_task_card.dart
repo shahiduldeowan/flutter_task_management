6f3f4845f3c2d3f9213e7f8dedaddda3c0755346
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-13 15:59:59 +0600

    feat: add BuildTaskCard and BuildTaskListView components; implement task status button and date formatting

---
+++ lib/presentation/widgets/shared/build_task_card.dart
@@ -0,0 +1,92 @@
+import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
+import "package:task/core/extensions/app_size_extension.dart";
+import "package:task/core/theme/app_decoration.dart";
+import "package:task/core/theme/theme_helper.dart";
+import "package:task/core/utils/date_formatter.dart";
+import "package:task/generated/assets.gen.dart";
+
+class BuildTaskCard extends StatelessWidget {
+  const BuildTaskCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(8.h),
+      decoration: AppDecoration.outlinedGrayBoxDecoration.copyWith(
+        borderRadius: BorderRadiusStyles.roundedBorder12,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Homepage Redesign", style: theme.textTheme.titleMedium),
+          Text(
+            "Redesign the homepage of our website to improve user engagement and align with our updated bran",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: theme.textTheme.bodySmall!.copyWith(height: 1.4),
+          ),
+          8.h.toHeightBox(),
+          Row(
+            children: [
+              _TaskDate(date: DateTime.now()),
+              const _TaskStatusButton(status: "Todo"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TaskDate extends StatelessWidget {
+  final DateTime date;
+
+  const _TaskDate({required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: [
+          SvgPicture.asset(AppAssets.svg.clockIcon.path,
+              height: 12.h, width: 12.h),
+          4.h.toWidthBox(),
+          Text(
+            DateFormatter.formatDateWithMonthDayYear(date),
+            style: theme.textTheme.bodySmall!.copyWith(fontSize: 10.fs),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TaskStatusButton extends StatelessWidget {
+  final String status;
+
+  const _TaskStatusButton({required this.status});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {}, // TODO: Implement action
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 6.h),
+        backgroundColor: const Color(0xFFF0EDFD),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusStyles.roundedBorder20,
+        ),
+      ),
+      child: Text(
+        status,
+        style: theme.textTheme.labelLarge!.copyWith(
+          color: theme.colorScheme.primary,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
