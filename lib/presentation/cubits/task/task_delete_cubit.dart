f504a8f43d7c09602dca0b13e4ee1362713abefe
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-13 11:14:43 +0600

    add exit label to localization and inject TaskDeleteCubit

--- lib/presentation/cubits/task/task_delete_cubit.dart
+++ lib/presentation/cubits/task/task_delete_cubit.dart
@@ -1,9 +1,11 @@
 import "package:equatable/equatable.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
+import "package:injectable/injectable.dart";
 import "package:task/domain/use_case/delete_task.dart";
 
 part "task_delete_state.dart";
 
+@injectable
 class TaskDeleteCubit extends Cubit<TaskDeleteState> {
   TaskDeleteCubit(this._deleteTaskUseCase) : super(TaskDeleteInitial());
 
