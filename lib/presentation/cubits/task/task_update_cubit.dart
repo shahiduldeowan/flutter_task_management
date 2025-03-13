7b21269b99eb692840b75af8d2f98883b4f3715f
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-13 11:13:57 +0600

    add task use cases and corresponding cubits to dependency injection

--- lib/presentation/cubits/task/task_update_cubit.dart
+++ lib/presentation/cubits/task/task_update_cubit.dart
@@ -1,11 +1,13 @@
 import "package:equatable/equatable.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
+import "package:injectable/injectable.dart";
 import "package:task/core/utils/app_localization_helper.dart";
 import "package:task/domain/entity/task.dart";
 import "package:task/domain/use_case/update_task.dart";
 
 part "task_update_state.dart";
 
+@injectable
 class TaskUpdateCubit extends Cubit<TaskUpdateState> {
   TaskUpdateCubit(this._updateTaskUseCase) : super(TaskUpdateInitial());
 
