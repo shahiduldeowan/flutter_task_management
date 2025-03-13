1ab1168bea029d5fa990c8b321ee33b46e785cad
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-13 15:59:31 +0600

    feat: add BuildTaskCard and BuildTaskListView; enhance date formatting utility and update HomePage layout

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -1,7 +1,17 @@
 import "package:intl/intl.dart";
 
 class DateFormatter {
-  static String formatDefault([DateTime? date]) {
+  /// Format a date as "dd MMM, yyyy" (e.g. "18 Nov, 2025").
+  ///
+  /// If [date] is null, the current date is used.
+  static String defaultDateFormat([DateTime? date]) {
     return DateFormat("dd MMM, yyyy").format(date ?? DateTime.now());
+  }
+
+  /// Format a date as "MMMM d, y" (e.g. "November 18, 2025").
+  ///
+  /// If [date] is null, the current date is used.
+  static String formatDateWithMonthDayYear([DateTime? date]) {
+    return DateFormat("MMMM d, y").format(date ?? DateTime.now());
   }
 }
