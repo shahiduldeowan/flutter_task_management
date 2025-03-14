93faf9cdcb3946bf5a7aaf00dece266840085ef9
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-14 08:52:10 +0600

    feat: update task repository methods to return Future<String> and add utility classes for loading indicators and empty states

---
+++ lib/core/extensions/text_form_field_style_extension.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+import "package:task/core/extensions/app_size_extension.dart";
+import "package:task/core/theme/app_decoration.dart";
+import "package:task/core/theme/theme_helper.dart";
+import "package:task/presentation/widgets/shared/input_field/build_custom_text_form_field.dart";
+
+extension TextFormFieldStyleExtension on BuildCustomTextFormField {
+  OutlineInputBorder get outlineGray => OutlineInputBorder(
+        borderRadius: BorderRadiusStyles.roundedBorder20,
+        borderSide: BorderSide(
+          color: appTheme.appBorderGay.withValues(
+            alpha: 0.49,
+          ),
+          width: 1,
+        ),
+      );
+
+  InputDecoration decoration({Widget? suffix}) => InputDecoration(
+        hintText: hintText ?? "",
+        hintStyle: hintStyle ?? theme.textTheme.bodySmall,
+        isDense: true,
+        contentPadding: contentPadding ?? EdgeInsets.all(14.h),
+        fillColor: theme.colorScheme.primaryContainer,
+        filled: true,
+        border: outlineGray,
+        enabledBorder: outlineGray,
+        focusedBorder: outlineGray,
+        suffix: suffix,
+        suffixIconConstraints: BoxConstraints(maxHeight: 48.h),
+      );
+}
