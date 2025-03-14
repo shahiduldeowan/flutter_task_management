bd8c463cfb370952f7417ced2a554e15eb1dea8a
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-14 19:46:57 +0600

    feat: add AppSizes constants for padding, font sizes, spacing, and border radius

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,24 @@
+class AppSizes {
+  // 🔹 Padding & Margin
+  static const double paddingXS = 6.0;
+  static const double paddingMM = 14.0;
+  static const double paddingL = 16.0;
+  static const double paddingXL = 20.0;
+  static const double paddingXXL = 24.0;
+
+  // 🔹 Font Sizes
+  static const double fontXS = 12.0;
+  static const double fontXL = 24.0;
+
+  // 🔹 Spacing (Gap between elements)
+  static const double spaceS = 8.0;
+  static const double spaceM = 12.0;
+  static const double spaceXL = 20.0;
+
+  // 🔹 Border Radius
+  static const double borderRadiusM = 12.0;
+  static const double borderRadiusMM = 14.0;
+  static const double borderRadiusL = 16.0;
+  static const double borderRadiusXL = 20.0;
+  static const double borderRadiusXXXL = 34.0;
+}
