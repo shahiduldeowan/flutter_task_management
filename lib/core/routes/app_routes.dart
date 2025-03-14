fc4c10b71b71461ab5f720282e34e88ebd21c237
Shahidul Deowan <deowanshahidul@gmail.com>
2025-03-14 08:52:23 +0600

    feat: update task repository methods to return Future<String> and implement utility classes for loading indicators and empty states

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,14 +1,19 @@
 import "package:flutter/material.dart";
+import "package:task/domain/entity/task.dart";
 import "package:task/presentation/pages/main_page.dart";
+import "package:task/presentation/pages/view_task_page.dart";
 
 class AppRoutes {
   AppRoutes._();
 
   static const String initialRoute = "/";
+  static const String viewTaskPageRoute = "/view_task_page/";
 
   static Route? onGenerateRoute(RouteSettings settings) {
     return switch (settings.name) {
       initialRoute => _mainPageBuilder(),
+      viewTaskPageRoute =>
+        _viewTaskPageBuilder(settings.arguments as TaskEntity),
       _ => null,
     };
   }
@@ -17,6 +22,14 @@ class AppRoutes {
     return MaterialPageRoute(
       builder: (mainPageContext) {
         return const MainPage();
+      },
+    );
+  }
+
+  static Route _viewTaskPageBuilder(TaskEntity task) {
+    return MaterialPageRoute(
+      builder: (viewTAskPageContext) {
+        return ViewTaskPage(task: task);
       },
     );
   }
